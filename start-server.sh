3c62ec3f27debdee19a711c19507dd980dcaddbc
MihirHundiwala <mihir.hundiwala@somaiya.edu>
2022-06-23 19:44:19 +0530

    Update start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-
-(cd src; gunicorn config.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) 
-&
-nginx -g "daemon off;"
+(cd src; gunicorn config.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) & nginx -g "daemon off;"
